2ee0d5c565c7b6e0569a67cb64ab983944e1aa4a
AmanyEl-Razaz <a_elrazaz94@yahoo.com>
2022-09-18 23:47:50 +0200

    Amany add admin class

--- cs16-project/main.cpp
+++ cs16-project/main.cpp
@@ -109,10 +109,80 @@ public:
     }
 };
 
-class Admin : public Person
+class Admin : public Employee
 {
-
+private :
+    int EmployeeCount;
+    Employee oneEmployee[100];
+public :
+void addEmployee {
+int id;
+string name, password;
+double balance;
+cout << "Please Enter Employee Name : "  ;
+cin >> name;
+cout << "Please Enter Employee ID : "  ;
+cin >> id;
+cout << "Please Enter Employee Password : "  ;
+cin >> password;
+cout << "Please Enter Employee balance : "  ;
+cin >> balance;
+Employee Employee (id,name,password,balance)
+}
+void editEmployee {
+cout << "Please enter the name of the employee you need to edit\n";
+string name;
+cin >> name;
+bool edited = false;
+for (int i =0; i < EmployeeCount; i++ ) {
+        if(Employee.get_name() == name){
+            edited = true;
+            edited = true;
+                cout << "Press 1 to edit name\n";
+                cout << "Press 2 to edit password\n";
+                cout << "Press 3 to edit salary\n";
+                int option;
+                cin >> option;
+                if(option == 1) {
+                    cout << "please enter the new name: ";
+                    cin >> name;
+                    if (checkName(name) == true)
+                    Employee[i].set_name(name);
+                    else
+                        cout<<"name must be all alphabetic chars " ;
+                } else if(option == 2) {
+                    string password;
+                    cout << "Please enter the new password : ";
+                    cin >> password;
+                    if (checkPassword(password) == true)
+                     Employee[i].set_password(password);
+                     else
+                        cout << "Password must be between 8 & 20 in size " ;
+                } else {
+                    int salary;
+                    cout << "Please enter the new salary: ";
+                    cin >> salary;
+                    if (checkSalary(salary) == true)
+                        Employee[i].setEmp_salary(salary);
+                    else
+                        cout << "salary must be minimum 5000 " ;
+                }
+                break;
+            }
+        }
+Admin() {EmployeeCount = 0;}
+}
+}
+void listAllEmployees() {
+        cout << "Starting to list all employees...\n";
+        for(int i = 0 ; i < EmployeeCount ; i++) {
+            oneEmployee[i].display();
+        }
+        cout << "Finished listing all employees...\n";
+    }
 };
+
+
 
 class Validation
 {
